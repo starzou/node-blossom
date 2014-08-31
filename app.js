ebe4503f26d21562320a861660932576fae0f20c
starzou <z260142514@qq.com>
2014-08-31 17:07:24 +0800

    封装

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ var bodyParser = require('body-parser');
 
 // my module
 var loader = require('./lib/loader');
+var error = loader.loadModule('error');
 
 // load routes
 var routes = loader.loadRoute('index');
 var users = loader.loadRoute('users');
+
+// load middleware
+var header = loader.loadMiddleware('header');
 
 var app = express();
 
@@ -28,40 +32,13 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(header());
+
 // routes setup
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+// error setup
+error.handlers(app);
 
 module.exports = app;
